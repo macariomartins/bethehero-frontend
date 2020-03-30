2fe1a4f7f9385e2b586fa92412918afaad112c1d
Macário Martins <macariomartinsjunior@gmail.com>
2020-03-30 19:47:58 -0300

    Enviando aplicação para o Heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    // baseURL: 'http://localhost:3333',
+    baseURL: 'https://bethehero-backend-macario.herokuapp.com/',
 });
 
 export default api;
